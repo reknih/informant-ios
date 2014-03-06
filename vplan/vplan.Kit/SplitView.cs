158c75522e54f6e9794965d1f6e3dabba73ceb45
Martin Haug <mhaug@live.de>
2014-03-06 19:56:12 +0100

    Moved.

---
+++ vplan/vplan.Kit/SplitView.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace vplan
+{
+	public class SplitView : UISplitViewController
+	{
+		UIViewController masterView, detailView;
+		SplitDelegate sd;
+
+		public SplitView () : base()
+		{
+			// create our master and detail views
+			masterView = new SecondViewController ();
+			detailView = new FirstViewController ();
+			sd = new SplitDelegate ();
+			Delegate = sd;
+			// create an array of controllers from them and then
+			// assign it to the controllers property
+			ViewControllers = new UIViewController[]
+			{ masterView, detailView }; // order is important
+		}
+
+		public override bool ShouldAutorotateToInterfaceOrientation
+		(UIInterfaceOrientation toInterfaceOrientation)
+		{
+			return true;
+		}
+		class SplitDelegate : UISplitViewControllerDelegate
+		{
+			public override bool ShouldHideViewController (UISplitViewController svc,
+				UIViewController viewController, UIInterfaceOrientation inOrientation)
+			{
+				return false;
+			}
+		}
+	}
+}
+
