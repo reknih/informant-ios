beb88f380e597384a29bcab4b9f06dc8f23b8b3b
Martin Haug <mhaug@live.de>
2014-07-19 21:57:55 +0200

    Keep messing with the cloud.

--- vplan/vplan.Kit/PrefManager.cs
+++ vplan/vplan.Kit/PrefManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading;
 using MonoTouch.Foundation;
+using MonoTouch.UIKit;
 
 namespace vplan
 {
@@ -7,13 +9,34 @@ namespace vplan
 	{
 		NSUbiquitousKeyValueStore store = NSUbiquitousKeyValueStore.DefaultStore;
 		NSUserDefaults locstore = new NSUserDefaults();
+		NSUrl iCloudUrl;
 		public PrefManager ()
 		{
+			checkICloud ();
 			refresh ();
 		}
 		protected void refresh () {
 			store.Synchronize ();
 			locstore.Synchronize ();
+		}
+		protected void checkICloud() {
+			// GetUrlForUbiquityContainer is blocking, Apple recommends background thread
+			new Thread(new ThreadStart(() => {
+				var uburl = NSFileManager.DefaultManager.GetUrlForUbiquityContainer(null);
+				// OR instead of null you can specify "TEAMID.com.xamarin.samples.icloud"
+				if (uburl == null) {
+					using(var pool = new NSAutoreleasePool())
+						pool.InvokeOnMainThread(()=> {
+						var alert = new UIAlertView("Keine i\uE049 verfügbar"
+							, "Deine Klasse wird auf deinem Gerät gespeichert!"
+							, null, "OK", null);
+						alert.Show ();
+					});
+				} else { // iCloud enabled, store the NSURL for later use
+					iCloudUrl = uburl;
+				}
+			})).Start();
+
 		}
 		public int getInt (string key) {
 			int val;
